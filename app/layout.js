0102ec550bbcf55322066910303ad3545163046a
Yalamber Subba <102561782+gutsyguy@users.noreply.github.com>
2024-08-18 19:51:49 -0700

    e

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs"; 
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
     <ClerkProvider>
     <html lang="en">
       <body className={inter.className} suppressContentEditableWarning suppressHydrationWarning>{children}</body>
+      <Analytics />
     </html>
     </ClerkProvider>
 
