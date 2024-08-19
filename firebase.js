1aa05c22c422db395ee907a12dd679046bc1ff1d
Yalamber Subba <102561782+gutsyguy@users.noreply.github.com>
2024-08-18 19:59:24 -0700

    e

--- firebase.js
+++ firebase.js
@@ -3,13 +3,13 @@ import {getFirestore} from 'firebase/firestore'
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAtCYSzG-fHtPh8qIuXDu32SnHS_xK78Lo",
-  authDomain: "flashcard-saas-f6eb6.firebaseapp.com",
-  projectId: "flashcard-saas-f6eb6",
-  storageBucket: "flashcard-saas-f6eb6.appspot.com",
-  messagingSenderId: "325781306031",
-  appId: "1:325781306031:web:1c0230d453fb523ff1b25a",
-  measurementId: "G-L0V1SVC21C"
+  apiKey: process.env.api,
+  authDomain: process.env.auth,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId 
 };
 
 
