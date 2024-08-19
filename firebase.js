63099b8f6043887342b0d9c60322f010accbe4af
Yalamber Subba <102561782+gutsyguy@users.noreply.github.com>
2024-08-18 19:26:33 -0700

    i

--- firebase.js
+++ firebase.js
@@ -1,14 +1,15 @@
 import { initializeApp } from "firebase/app";
 import {getFirestore} from 'firebase/firestore'
 
+
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: "flashcard-saas-f6eb",
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID 
+  apiKey: "AIzaSyAtCYSzG-fHtPh8qIuXDu32SnHS_xK78Lo",
+  authDomain: "flashcard-saas-f6eb6.firebaseapp.com",
+  projectId: "flashcard-saas-f6eb6",
+  storageBucket: "flashcard-saas-f6eb6.appspot.com",
+  messagingSenderId: "325781306031",
+  appId: "1:325781306031:web:1c0230d453fb523ff1b25a",
+  measurementId: "G-L0V1SVC21C"
 };
 
 
